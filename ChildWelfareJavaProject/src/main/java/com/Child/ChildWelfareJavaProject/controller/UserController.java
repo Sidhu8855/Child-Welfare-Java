3a863e33ce1ddf846ae3f684c6d80f1f847980d7
Siddesh <siddeshlonari01@gmail.com>
2024-04-20 11:16:55 +0530

    raje yogesh

--- ChildWelfareJavaProject/src/main/java/com/Child/ChildWelfareJavaProject/controller/UserController.java
+++ ChildWelfareJavaProject/src/main/java/com/Child/ChildWelfareJavaProject/controller/UserController.java
@@ -3,7 +3,6 @@ package com.Child.ChildWelfareJavaProject.controller;
 import java.io.IOException;
 import java.util.List;
 
-
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -13,16 +12,17 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
-
 import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.multipart.MultipartFile;
 
 import com.Child.ChildWelfareJavaProject.Entity.Child;
 import com.Child.ChildWelfareJavaProject.Entity.User;
+import com.Child.ChildWelfareJavaProject.Entity.UserChild;
 import com.Child.ChildWelfareJavaProject.Responce.Responce;
 import com.Child.ChildWelfareJavaProject.dto.LoginRequest;
 import com.Child.ChildWelfareJavaProject.repositry.ChildRepositry;
+import com.Child.ChildWelfareJavaProject.repositry.UserChildrepo;
 import com.Child.ChildWelfareJavaProject.service.UserService;
 
 //@CrossOrigin(origins = "http://localhost:8100")
@@ -31,80 +31,120 @@ import com.Child.ChildWelfareJavaProject.service.UserService;
 @Controller
 
 public class UserController {
-	
+
 	@Autowired
-    private	UserService userService;
+	private UserService userService;
+
 	@PostMapping("/save")
-	public Responce save(@RequestBody User user)
-	{
-		return userService.save(user); 
+	public Responce save(@RequestBody User user) {
+		return userService.save(user);
 	}
-	
+
 	@PostMapping("/login")
-	public Responce login(@RequestBody LoginRequest loginRequest)
-	{
+	public Responce login(@RequestBody LoginRequest loginRequest) {
 		return userService.loginUser(loginRequest);
 	}
-	
-	
+
 	@Autowired
 	ChildRepositry childRepository;
-	@PostMapping("postapi")
-	 public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file,
-             @RequestParam("studentname") String studentname,
-             @RequestParam("age") Integer age,
-             @RequestParam("location") String location,
-             @RequestParam("gender") String gender,
-             @RequestParam("bloodgroup") String bloodgroup) {
-		if (file.isEmpty()) {
-		return new ResponseEntity<>("Please select a file to upload", HttpStatus.BAD_REQUEST);
-		}
-		
-		try {
-		// Create a new Child object
-		Child child = new Child();
-		child.setStudentname(studentname);
-		child.setAge(age);
-		child.setLocation(location);
-		child.setGender(gender);
-		child.setBloodgroup(bloodgroup);
-		child.setImgname(file.getOriginalFilename());
-		child.setStudentimages(file.getBytes());
-		
-		// Save the Child object to the repository
-		childRepository.save(child);
-		
-		return new ResponseEntity<>("File uploaded successfully", HttpStatus.OK);
-		} catch (IOException e) {
-		e.printStackTrace();
-		return new ResponseEntity<>("Failed to upload file", HttpStatus.INTERNAL_SERVER_ERROR);
-		}
-}
-	
-	
 
-	
-	
-    @GetMapping("/{id}")
-    public ResponseEntity<Child> getChildById(@PathVariable Long id) {
-        return childRepository.findById(id)
-                .map(child -> ResponseEntity.ok().body(child))
-                .orElseGet(() -> ResponseEntity.notFound().build());
-    }
-    
-        
-  
-    @GetMapping("children")
-    public ResponseEntity<List<Child>> getAllChildren() {
-        List<Child> children = childRepository.findAll();
-        return ResponseEntity.ok().body(children);
-    }
-    
-   
-    	public void getnewemp() {
-		
+	@PostMapping("postapi")
+	public ResponseEntity<String> uploadImage(@RequestParam("file") MultipartFile file,
+			@RequestParam("studentname") String studentname, @RequestParam("age") Integer age,
+			@RequestParam("location") String location, @RequestParam("gender") String gender,
+			@RequestParam("bloodgroup") String bloodgroup) {
+		if (file.isEmpty()) {
+			return new ResponseEntity<>("Please select a file to upload", HttpStatus.BAD_REQUEST);
+		}
+
+		try {
+			// Create a new Child object
+			Child child = new Child();
+			child.setStudentname(studentname);
+			child.setAge(age);
+			child.setLocation(location);
+			child.setGender(gender);
+			child.setBloodgroup(bloodgroup);
+			child.setImgname(file.getOriginalFilename());
+			child.setStudentimages(file.getBytes());
+
+			// Save the Child object to the repository
+			childRepository.save(child);
+
+			return new ResponseEntity<>("File uploaded successfully", HttpStatus.OK);
+		} catch (IOException e) {
+			e.printStackTrace();
+			return new ResponseEntity<>("Failed to upload file", HttpStatus.INTERNAL_SERVER_ERROR);
+		}
 	}
-	
-	
+
+	@GetMapping("/{id}")
+	public ResponseEntity<Child> getChildById(@PathVariable Long id) {
+		return childRepository.findById(id).map(child -> ResponseEntity.ok().body(child))
+				.orElseGet(() -> ResponseEntity.notFound().build());
+	}
+
+	@GetMapping("children")
+	public ResponseEntity<List<Child>> getAllChildren() {
+		List<Child> children = childRepository.findAll();
+		return ResponseEntity.ok().body(children);
+	}
+
+	public void getnewemp() {
+
+	}
+
+
+	@Autowired
+    private UserChildrepo userchildrepo;
+
+//    @PostMapping("/uploadUserChild")
+//    public ResponseEntity<String> uploadUserChild(
+//            @RequestParam("file") MultipartFile file,
+//            @RequestParam("location") String location,
+//            @RequestParam("description") String description) {
+//        if (file.isEmpty()) {
+//            return new ResponseEntity<>("Please select a file to upload", HttpStatus.BAD_REQUEST);
+//        }
+//
+//        try {
+//            UserChild userchild = new UserChild();
+//            userchild.setLocation(location);
+//            userchild.setDescription(description);
+//            userchild.setImageName(file.getOriginalFilename());
+//            userchild.setChildImages(file.getBytes());
+//
+//            userchildrepo.save(userchild);
+//
+//            return new ResponseEntity<>("File uploaded successfully", HttpStatus.OK);
+//        } catch (IOException e) {
+//            e.printStackTrace();
+//           return new ResponseEntity<>("Failed to upload file",HttpStatus.INTERNAL_SERVER_ERROR);
+//        }
+//    }
+	@PostMapping("/uploadUserChild")
+	public ResponseEntity<String> uploadUserChild(
+	        @RequestParam("file") MultipartFile file,
+	        @RequestParam("location") String location,
+	        @RequestParam("description") String description) {
+	    if (file.isEmpty()) {
+	        return new ResponseEntity<>("Please select a file to upload", HttpStatus.BAD_REQUEST);
+	    }
+
+	    try {
+	        UserChild userchild = new UserChild();
+	        userchild.setLocation(location);
+	        userchild.setDescription(description);
+	        userchild.setImageName(file.getOriginalFilename());
+	        userchild.setChildImages(file.getBytes());
+
+	        userchildrepo.save(userchild);
+
+	        return new ResponseEntity<>("File uploaded successfully", HttpStatus.OK);
+	    } catch (IOException e) {
+	        e.printStackTrace();
+	       return new ResponseEntity<>("Failed to upload file",HttpStatus.INTERNAL_SERVER_ERROR);
+	    }
+	}
 
 }
---
+++ ChildWelfareJavaProject/src/main/java/com/Child/ChildWelfareJavaProject/repositry/UserChildrepo.java
@@ -0,0 +1,9 @@
+package com.Child.ChildWelfareJavaProject.repositry;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+
+import com.Child.ChildWelfareJavaProject.Entity.UserChild;
+
+public interface UserChildrepo extends JpaRepository<UserChild,Long>{
+
+}
